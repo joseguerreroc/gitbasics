665a6ed0a1601fbbea79bfbbae315c8b746a6011
José Guerrero <neuroppgc@gmail.com>
2016-03-03 21:11:11 -0600

    Prueba para subir un segundo archivo

---
+++ recursion1.py
@@ -0,0 +1,16 @@
+# Punto de terminacion
+
+# [1, 2, 3, 4, 6]
+
+# elem_0 + suma del resto
+	# primer 
+
+# Suma de una lista de un elemento es el elemento mismo
+
+def suma(lista):
+	if lista:
+		return lista[0] + lista(lista[1:])
+	else:
+		0
+
+print(suma([1, 2, 3 , 4])
